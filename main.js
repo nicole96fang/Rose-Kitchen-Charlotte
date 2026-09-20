4fae28701a9d0c19ae6b034ee233adaad4bf1e3f
nicole96fang <72725057+nicole96fang@users.noreply.github.com>
2026-09-20 16:54:41 +0800

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import "./styles.css";
-import { createStore } from "./store.js";
-import { createApp } from "./app.js";
-
-const store = createStore();
-createApp(document.getElementById("app"), store);
